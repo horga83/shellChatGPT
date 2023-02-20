a9b195e6922d5a86bf734e1fdb54ae84ab4708f1
jsonijr@gmail.com <netosj@yahoo.com>
2023-02-20 20:10:25 -0300

    Much improved chat mode.
    Some of the updates for chat mode in v0.4.1:
    - Total integration amongst positional arguments, visual editor, and chatbot loop
    - Set/unset some configurations with flags/commands as input prompt on the run
    - Improved history file recording
    - Interlocutor may be set explicitly such as `INTERLOCUTOR_NAME: [text]`
    - Freedom to make up your own prompts for the chatbot with `: [intructions]`
    - Verbose can be reduced with -v (beware that disables prompt confirmation prompts)

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env ksh
-# chatgpt.sh -- Ksh/Bash ChatGPT Shell Wrapper
-# v0.3.4  2023  by mountaineerbr  GPL+3
+# chatgpt.sh -- Ksh93/Bash ChatGPT Shell Wrapper
+# v0.4.1  2023  by mountaineerbr  GPL+3
 [[ $BASH_VERSION ]] && shopt -s extglob
+[[ $ZSH_VERSION  ]] && setopt NO_SH_GLOB KSH_GLOB KSH_ARRAYS SH_WORD_SPLIT GLOB_SUBST
 
 # OpenAI API key
 #OPENAI_KEY=
@@ -83,44 +84,74 @@ COMPLETIONS
 	completions, and can also return the probabilities of
 	alternative tokens at each position.
 
+	To keep a history of the latest context in the chat, set option
+	-c. This starts a new session, keeps a record of the latest
+	prompts and replies, and sends some history context with new
+	questions. This option respects max tokens setting. Set -C to
+	continue from last recorded session.
+
+	The defaults chat format is \`Q & A'. A name such as \`NAME:'
+	may be introduced as interlocutor. Setting \`:' will not add
+	and interlocutor to the following text, this may be useful to
+	set intructions, and completing a previous prompt.
+
+	While in chat mode option -c, type in one of the following in
+	the new prompt to set options on the go:
+
+		-c  |  !new 	  Starts new session.
+		-x  |  !editor 	  Set/unset text editor.
+		-v  |  !verbose	  Set/unset verbose.
+		-V  |  !block	  Print prompt.
+		!q  |  !quit	  Exit.
+
+
+	Prompt Design
 	Make a good prompt. May use bullets for multiple questions in
 	a single prompt. Write \`act as [technician]', add examples of
 	expected results.
 
-	To keep a history of the latest context in the chat, set option
-	-c. This keeps a record of the latest prompts and replies and
-	sends some history context with new questions. This option
-	respects max tokens setting. Set -C to break from previous
-	session.
+	For the chatbot, the only indication given is the initial \`Q:'
+	interlocutor flag so the initial reply may vary considerably.
+	You may try setting the bot identity and how it behaves as
+	intructions at the first prompt, such as:
 
-	The chat format is \`Q: [prompt]' and \`A: [reply]', but a single
-	letter such as \`I:' or simply \`:' may be set to mark the initial
-	instruction prompt.
+	prompt>	\": The following is a conversation with an AI assistant.
+		   The assistant is helpful, creative, clever, and very
+		   friendly.\"
+
+	reply_> \"Assistant: Hello! How can I help you?\"
+
+	prompt> \"Human: Hello, what is your name?\"
 
 
+	For more on prompt design, see
+	<https://platform.openai.com/docs/guides/completion/prompt-design>.
+
+
+	Settings
 	Temperature 	number 	Optional 	Defaults to $OPTT
+
 	Lowering temperature means it will take fewer risks, and
 	completions will be more accurate and deterministic. Increasing
 	temperature will result in more diverse completions.
-	
 	Ex: low-temp:  We’re not asking the model to try to be creative
 	with its responses – especially for yes or no questions.
 
 
 	Top_p 	number 	Optional 	Defaults to $OPTP
+	
 	An alternative to sampling with temperature, called nucleus
 	sampling, where the model considers the results of the tokens
 	with top_p probability mass. So 0.1 means only the tokens
 	comprising the top 10% probability mass are considered.
-
 	They generally recommend altering this or temperature but both.
 
 	
 	Presence_penalty 	number 	Optional 	Defaults to 0
 	Frequency_penalty 	number 	Optional 	Defaults to 0
+
 	Number between -2.0 and 2.0. Positive values penalize new tokens
 	based on whether they appear in the text so far.
-
 	Presense penalty increases the model's likelihood to talk about
 	new topics, while frequency penalty decreases the model's like-
 	lihood to repeat the same line verbatim.
@@ -136,14 +167,12 @@ IMAGES
 	The first positional parameter sets the output image size
 	256x256/small, 512x512/medium or 1024x1024/large. Defaults=$OPTS.
 
-	CREATE IMAGE
-	Creates an image given a prompt. A text description of the
-	desired image(s). The maximum length is 1000 characters.
+	An image can be created given a prompt. A text description of
+	the desired image(s). The maximum length is 1000 characters.
 
-	IMAGE VARIATION
-	Creates a variation of a given image. The image to use as
-	the basis for the variation(s). Must be a valid PNG file,
-	less than 4MB and square. If Imagemagick is available,
+	Also, a variation of a given image can be generated. The image
+	to use as the basis for the variation(s). Must be a valid PNG
+	file, less than 4MB and square. If Imagemagick is available,
 	input image will be converted to square before upload.
 
 
@@ -156,11 +185,6 @@ ENVIRONMENT
 			Defaults=vim
 	
 	OPENAI_KEY 	Set your personal (free) OpenAI API key.
-
-
-REQUIREMENTS
-	A free OpenAI GPTChat key. Ksh or Bash. cURL. JQ and
-	ImageMagick are optionally required.
 
 
 LIMITS
@@ -177,19 +201,25 @@ LIMITS
 
 
 BUGS
-	A PROMPT may return an empty response. Try trimming ending spaces,
-	appending a full stop/ellipsis, or resetting temperature. Maybe
-	the model just does not have anything else to add/complete.
+	Certain PROMPTS may return empty responses. Maybe the model
+	has nothing to add to the input prompt or it expects mor text.
+	Try trimming spaces, appending a full stop/ellipsis, or
+	resetting temperature or adding more text. See prompt deesign.
+
+
+REQUIREMENTS
+	A free OpenAI GPTChat key. Ksh93 or Bash. cURL. JQ and
+	ImageMagick are optionally required.
 
 
 OPTIONS
 	-NUM 		Set maximum tokens. Max=4096, defaults=$OPTMM.
 	-a [VAL]	Set presence penalty  (completions; -2.0 - 2.0).
 	-A [VAL]	Set frequency penalty (completions; -2.0 - 2.0).
-	-c 		Set chat mode, read history file (completions).
-	-C 		Set chat mode and start new session in history file.
+	-cc 		Chat mode, new session (completions). Set twice
+			to continue from last history session.
 	-e [INSTRUCT] [INPUT]
-			Set Edit mode, defaults to text-davinci-edit-001.
+			Set Edit mode, model defaults=text-davinci-edit-001.
 	-h 		Print this help page.
 	-i [PROMPT] 	Creates an image given a prompt.
 	-i [PNG_PATH] 	Creates a variation of a given image.
@@ -209,9 +239,9 @@ OPTIONS
 	-n [NUM] 	Set number of results. Defaults=$OPTN.
 	-p [VAL] 	Set top_p value (0.0 - 1.0). Defaults=$OPTP.
 	-t [VAL] 	Set temperature value (0.0 - 2.0). Defaults=$OPTT.
-	-vv 		Print request body, set twice to dump and exit.
-	-xx 		Edit current prompt in text editor, set twice
-			to edit previous prompt buffer.
+	-v 		Less verbose in chat mode.
+	-VV 		View request body. Set twice to dump and exit.
+	-x 		Edit prompt in text editor.
 	-z 		Print last call JSON file backup."
 
 MODELS=(
@@ -242,40 +272,54 @@ ENDPOINTS=(
 	
 
 
-function prompt_mainf
+function promptf
 {
-	curl -\# -L https://api.openai.com/v1/${ENDPOINTS[$EPN]} \
+	((OPTVV)) && ((!OPTII)) && { 	block_printf ;return ;}
+	
+	curl -\# ${OPTV:+-s} -L https://api.openai.com/v1/${ENDPOINTS[$EPN]} \
 		-H "Content-Type: application/json" \
 		-H "Authorization: Bearer $OPENAI_KEY" \
 		-d "$BLOCK" \
 		-o "$FILE"
 }
 
-function promptf
+function block_printf
 {
-	if ((OPTV>1))
+	if ((OPTVV>1))
 	then 	printf '%s\n' "$BLOCK" ;exit
-	elif ((OPTV))
-	then	jq -r '.instruction//empty, .input//empty, .prompt//empty' <<<"$BLOCK" || printf '%s\n' "$BLOCK"
+	else	jq -r '.instruction//empty, .input//empty, .prompt//empty' <<<"$BLOCK" || printf '%s\n' "$BLOCK"
 	fi
-	prompt_mainf "$@"
+}
+
+function new_prompt_confirmf
+{
+	typeset REPLY
+	((OPTV)) && return
+
+	printf '%s \n' "Confirm prompt? [Y]es, [n]o,${OPTX:+ [e]dit,} [r]edo or [a]bort" >&2
+	read -r -n 1
+	case "${REPLY:-$1}" in
+		[AaQq]*) 	return 201;;  #break
+		[Rr]*) 	return 200;;  #continue
+		[EeVv]*) 	return 199;;  #edf
+		[Nn]*) 	unset REC_OUT TKN_PREV ;return 1;;  #no
+	esac  #yes
 }
 
 function prompt_printf
 {
 	if ((OPTJ)) #print raw json
 	then 	cat -- "$FILE"
-	else 	jq -r '"Model_: \(.model//"?") (\(.object//"?"))",
+	else 	((OPTV)) || jq -r '"Model_: \(.model//"?") (\(.object//"?"))",
 			"Usage_: \(.usage.prompt_tokens) + \(.usage.completion_tokens) = \(.usage.total_tokens//empty) tokens"' "$FILE" >&2
 		jq -r '.choices[1] as $sep | .choices[] | (.text, if $sep != null then "---" else empty end)' "$FILE" \
-		|| jq -r '.choices[].text' "$FILE" \
-		|| cat -- "$FILE"
+		|| jq -r '.choices[].text' "$FILE" || cat -- "$FILE"
 	fi
 }
 
 function prompt_imgvarf
 {
-	curl -\# -L https://api.openai.com/v1/${ENDPOINTS[$EPN]} \
+	curl -\# ${OPTV:+-s} -L https://api.openai.com/v1/${ENDPOINTS[$EPN]} \
 		-H "Authorization: Bearer $OPENAI_KEY" \
 		-F image="@$1" \
 		-F response_format="$OPTI_FMT" \
@@ -299,7 +343,7 @@ function prompt_imgprintf
 		while jq -e ".data[${n}]" "$FILE" >/dev/null 2>&1
 		do 	fout="${FILEOUT%.*}${m}.png"
 			jq -r ".data[${n}].b64_json" "$FILE" | base64 -d > "$fout"
-			printf '%s\n' "File: ${fout/$HOME/\~}" >&2
+			printf 'File: %s\n' "${fout/$HOME/\~}" >&2
 			((++n, ++m)) ;((n<50)) || break
 		done
 		((n)) || { 	cat -- "$FILE" ;false ;}
@@ -328,34 +372,83 @@ function lastjsonf
 function token_prevf
 {
 	TKN_PREV=$(($(wc -c <<<"$*")/4))
-	printf '%s\n' "Prompt tokens: ~$TKN_PREV; Max tokens: $OPTMAX" >&2
+	printf 'Prompt tokens: ~%d; Max tokens: %d\n' "$TKN_PREV" "$OPTMAX" >&2
 }
 
 function check_typef
 {
-	[[ $* = ?(*+(\\n|$'\n'))*([$IFS\"])*([A-Za-z0-9/.+-]):* ]]
+	TYPE_SPC1="?(*+(\\\\n|$'\n'))*([$IFS\"])"
+	TYPE_GLOB="*([A-Za-z0-9@_/.+-])"
+	TYPE_SPC2="*(\\\\t|[$' \t'])"
+	TYPE_SPC3="*(\\\\[nt]|[$' \n\t'])"
+	[[ $* = $TYPE_SPC1$TYPE_GLOB$TYPE_SPC2:$TYPE_SPC3* ]]
+}
+function set_typef
+{
+	check_typef "$*" || return
+	USER_TYPE="$*"
+	USER_TYPE="${USER_TYPE%%:*}"
+	USER_TYPE="${USER_TYPE%%$TYPE_SPC2}"
+	USER_TYPE="${USER_TYPE##$TYPE_SPC1}"
+}
+
+function check_cmdf
+{
+	case "${*//[$IFS]}" in
+		[+-][Cc]|!br|!break|!session)
+			break_sessionf ;return 0
+			;;
+		-x|!ed|!editor)
+			((OPTX)) && unset OPTX || OPTX=1 ;return 0
+			;;
+		-v|!ver|!verbose)
+			((OPTV)) && unset OPTV || OPTV=1 ;return 0
+			;;
+		-V|!blk|!block)
+			((OPTVV)) && unset OPTVV || OPTVV=1 ;return 0
+			;;
+		-VV|!!blk|!!block)
+			OPTVV=2 ;return 0
+			;;
+		!q|!quit|!exit|!bye)
+			exit
+			;;
+	esac
+	return 1
 }
 
 function edf
 {
-	typeset pre pos REPLY
+	typeset ed_msg pre pos REPLY
 	
-	((OPTX<2)) && (($#)) && unescapef "$@" >"$FILETXT"
-	((OPTC)) && ((OPTX)) && pre=$(<"$FILETXT")
+	if ((OPTC))
+	then 	ed_msg=",,,,,,(edit below this line),,,,,,"
+		pre=$(unescapef "$HIST${HIST:+\\n$ed_msg}")
+		printf "%s${pre:+\\n}" "$pre" >"$FILETXT"
+		if (($#))
+		then 	printf "${pre:+\\n}%s\n" "$*"
+		else 	printf "${pre:+\\n}%s: \n" "${USER_TYPE:-Q}"
+		fi >>"$FILETXT"
+	fi
 	
 	${VISUAL:-${EDITOR:-vim}} "$FILETXT" </dev/tty >/dev/tty
-	
-	printf '%s\n' "Confirm new prompt? [Y]es, [n]o or [a]bort " >&2
-	if read -r -n 1 ;[[ $REPLY = [AaEeQq] ]]
-	then 	exit 2
-	elif [[ $REPLY = [Nn] ]]
-	then 	return 1
-	elif ((OPTC)) && ((OPTX)) &&
-		pos=$(<"$FILETXT") && [[ "$pos" != "$pre" ]]
-	then 	check_typef "${pos#*"$pre"}" &&
-		REC_OUT="${pos#*"$pre"}" || REC_OUT="Q: ${pos#*"$pre"}"
+
+	if ((OPTC)) && pos=$(<"$FILETXT") && [[ "$pos" != "$pre" ]]
+	then 	if [[ "$pos" != "$pre"* ]]
+		then
+			printf 'Warning: %s \n' 'Bad edit. [R]edit, r[e]do or [c]ontinue?' >&2
+			read -r
+			case "$REPLY" in
+				[CcNnQqAa]) 	:;;
+				[Ee]*) return 200;;
+				[Rr]|*) OPTC= edf "$@" || return ;pos=$(<"$FILETXT");;
+			esac
+			#case ${REPLY:-$1} in 	[!NnQq]*) return 200;; esac
+		fi
+		set -- "${pos#*"$pre"}"
+		check_cmdf "$*" && return 200
+		set_typef "$*" && REC_OUT="$*" || REC_OUT="${USER_TYPE:-Q}: $*"
 	fi
-	((OPTC)) && token_prevf "${pos#*"$pre"}"
 	return 0
 }
 
@@ -375,16 +468,22 @@ function unescapef
 	printf '%s\n' "$@"
 }
 
+function break_sessionf
+{
+	[[ -e "$FILECHAT" ]] || return
+	[[ $(<"$FILECHAT") = *[Bb][Rr][Ee][Aa][Kk] ]] ||
+	tee -a -- "$FILECHAT" >&2 <<<'SESSION BREAK'
+}
+
 
 #parse opts
-while getopts a:A:cCehiIjlm:n:kp:t:vxz0123456789 c
+while getopts a:A:cehiIjlm:n:kp:t:vxz0123456789 c
 do 	[[ $OPTARG = .[0-9]* ]] && OPTARG=0$OPTARG
 	case $c in
 		[0-9]) 	OPTMAX=$OPTMAX$c;;
 		a) 	OPTA="$OPTARG";;
 		A) 	OPTAA="$OPTARG";;
-		c) 	OPTC=1;;
-		C) 	((OPTCC++)) || tee -a -- "${FILECHAT}" >&2 <<<'SESSION BREAK'; OPTC=1;;
+		c) 	((OPTC++));;
 		e) 	OPTE=1;;
 		h) 	printf '%s\n' "$HELP" ;exit ;;
 		i|I) 	OPTI=1;;
@@ -398,16 +497,18 @@ do 	[[ $OPTARG = .[0-9]* ]] && OPTARG=0$OPTARG
 		n) 	OPTN=$OPTARG ;;
 		k) 	OPENAI_KEY=$OPTARG;;
 		p) 	if ((OPTARG>1))
-			then 	printf '%s\n' "err: illegal top_p -- $OPTARG" >&2
+			then 	printf 'err: illegal top_p -- %s\n' "$OPTARG" >&2
 			else 	OPTP=$OPTARG
 			fi;;
 		t) 	if ((OPTARG>2))
-			then 	printf '%s\n' "err: illegal temperature -- $OPTARG" >&2
+			then 	printf 'err: illegal temperature -- %s\n' "$OPTARG" >&2
 			else 	OPTT=$OPTARG
 			fi;;
 		v) 	((++OPTV));;
-		x) 	((++OPTX));;
+		V) 	((++OPTVV));;
+		x) 	OPTX=1;;
 		z) 	OPTZ=1;;
+		\?) 	exit 1;;
 	esac
 done ; unset c
 shift $((OPTIND -1))
@@ -449,7 +550,7 @@ esac
 
 (($#)) || [[ -t 0 ]] || set -- "$(</dev/stdin)"
 ((OPTX)) && ((!OPTC)) && edf "$@" && set -- "$(<"$FILETXT")"  #editor
-((OPTI+OPTII+OPTL+OPTZ)) || token_prevf "$*"
+((OPTI+OPTII+OPTL+OPTZ)) || ((!$#)) || token_prevf "$*"
 for arg  #escape input
 do 	((init++)) || set --
 	set -- "$@" "$(escapef "$arg")"
@@ -507,63 +608,103 @@ then 	: "${2:?EDIT MODE ERR}"
 	promptf
 	prompt_printf
 else               #completions
-	if [[ $OPTC ]]  #chat mode
-	then 	check_typef "$*" && set -- "${*##*([$IFS:])}" || set -- "Q: ${*##*([$IFS:])}"
-		REC_OUT="$*"
-		if [[ -s "${FILECHAT}" ]]
-		then 	((max_prev=TKN_PREV+1))
-			while IFS=$'\t' read -r time token string
-			do 	[[ $time$token = *[Bb][Rr][Ee][Aa][Kk]* ]] && break
-				[[ ${string//[$IFS\"]} ]] && ((token>0)) || continue
-				if ((max_prev+token+1<OPTMAX))
-				then 	((max_prev+=token+1))
-					string="${string#[ \"]}" string="${string%[ \"]}"
-					set -- "${string#[ :]}\n\n$*"
-				fi
-			done < <(tac -- "${FILECHAT}")
-			((max_prev-=TKN_PREV+1))
-			unset REPLY time token string
-		fi
-		((OPTX)) && OPTX=1  edf "$@" && set -- "$(escapef "$(<"$FILETXT")")"
-		if [[ ${*//[$IFS\"]} = *(*([A-Za-z0-9/.+-]):) ]] \
-			|| [[ ${REC_OUT//[$IFS\"]} = *(*([A-Za-z0-9/.+-]):) ]]
-		then 	printf '%s\n' "Enter prompt: " >&2
-			read -r ${BASH_VERSION:+-e}
-			if [[ $REPLY ]]
-			then 	{ 	check_typef "$REPLY" && set -- "$*${REPLY##*([$IFS:])}" ;} || set -- "$*Q: ${REPLY##*([$IFS:])}"
-			else 	set --  #err on empty input later
+	((!OPTC)) || ((OPTC>1)) || break_sessionf
+	while :
+	do 	if [[ $OPTC ]]  #chat mode
+		then 	if (($#))  #input from pos args, first pass
+			then 	check_cmdf "$*" && continue
+				set_typef "$*" && REC_OUT="$*" \
+					|| REC_OUT="${USER_TYPE:-Q}: $*"
+				set -- "$REC_OUT"
 			fi
-			unset REC_OUT
+
+			#read hist file
+			if [[ -s "${FILECHAT}" ]]
+			then 	((max_prev=TKN_PREV+1))
+				while IFS=$'\t' read -r time token string
+				do 	[[ $time$token = *[Bb][Rr][Ee][Aa][Kk]* ]] && break
+					[[ ${string//[$IFS\"]} ]] && ((token>0)) || continue
+					if ((max_prev+token+1<OPTMAX))
+					then 	((max_prev+=token+1))
+						string="${string#[ \"]}" string="${string%[ \"]}"
+						HIST="${string#[ :]}\n\n$HIST"
+					fi
+				done < <(tac -- "$FILECHAT")
+				((max_prev-=TKN_PREV+1))
+				unset REPLY time token string
+			fi
+
+			#text editor
+			if ((OPTX))
+			then 	edf "$@"
+				while :
+				do 	new_prompt_confirmf
+					case $? in
+						201) 	break 2;;  #abort
+						200) 	continue 2;;  #redo
+						199) 	OPTC= edf "$@" || break 2;;  #edit
+						0) 	set -- "$(escapef "$(<"$FILETXT")")"
+							break;;  #yes
+						*) 	break;;  #no
+					esac
+				done
+			fi
+
+			#fallback prompt read
+			if [[ ${*//[$IFS\"]} = *($TYPE_GLOB:) ]] \
+				|| [[ ${REC_OUT//[$IFS\"]} = *($TYPE_GLOB:) ]]
+			then 	while :
+				do 	printf '\n%s[%s]: ' "Prompt" "${USER_TYPE:-Q}" >&2
+					read -r ${BASH_VERSION:+-e}
+					if [[ $REPLY ]]
+					then 	check_cmdf "$REPLY" && continue
+						OPTX= new_prompt_confirmf
+						case $? in
+							201) 	break 2;;  #abort
+							200|199) 	continue;;  #redo/edit
+							0) 	:;;  #yes
+							*) 	unset REPLY; set -- ;break;;  #no
+						esac
+						set_typef "$REPLY" && REC_OUT="$REPLY" \
+							|| REC_OUT="${USER_TYPE:-Q}: $REPLY"
+						set -- "$HIST$REC_OUT"
+					else 	set --
+					fi ;break
+				done
+			fi
 		fi
-	fi
-	#https://thoughtblogger.com/continuing-a-conversation-with-a-chatbot-using-gpt/
+		#https://thoughtblogger.com/continuing-a-conversation-with-a-chatbot-using-gpt/
 
-	BLOCK="{
-		\"model\": \"$MOD\",
-		\"prompt\": \"${*:?PROMPT ERR}\",
-		\"temperature\": $OPTT,
-		\"top_p\": $OPTP, $OPTA_OPT $OPTAA_OPT
-		\"max_tokens\": $OPTMAX,
-		\"n\": $OPTN
-	}"
-	promptf
-	prompt_printf
+		BLOCK="{
+			\"model\": \"$MOD\",
+			\"prompt\": \"${*:?PROMPT ERR}\",
+			\"temperature\": $OPTT,
+			\"top_p\": $OPTP, $OPTA_OPT $OPTAA_OPT
+			\"max_tokens\": $OPTMAX,
+			\"n\": $OPTN
+		}"
+		promptf
+		prompt_printf
 
-	if [[ $OPTC ]] && {
-	 	tkn=($(jq -r '.usage.prompt_tokens//empty,
-			.usage.completion_tokens//empty,
-			(.created//empty|strflocaltime("%Y-%m-%dT%H:%M:%S%Z"))' "$FILE"
-		) )
-		ans=$(jq '.choices[0].text' "$FILE") ans="${ans##*(\\[nt]|\")}" ans="${ans%\"}"
-		((${#tkn[@]}>2)) && ((${#ans}))
-		}
-	then 	{ 	check_typef "$ans" || ans="A: $ans"
-			printf '%s\t%d\t"%s"\n' "${tkn[2]}" "$((max_prev<=tkn[0]?tkn[0]-max_prev:-1))" "$(escapef "${REC_OUT:-$*}")"
-			printf '%s\t%d\t"%s"\n' "${tkn[2]}" "${tkn[1]}" "$ans"
-		} >> "${FILECHAT}"
-	fi; unset tkn ans
+		#record to hist file
+		if [[ $OPTC ]] && {
+		 	tkn=($(jq -r '.usage.prompt_tokens//empty,
+				.usage.completion_tokens//empty,
+				(.created//empty|strflocaltime("%Y-%m-%dT%H:%M:%S%Z"))' "$FILE"
+			) )
+			ans=$(jq '.choices[0].text' "$FILE")
+			ans="${ans##*([$IFS]|\\[nt]|\")}" ans="${ans%\"}"
+			((${#tkn[@]}>2)) && ((${#ans}))
+			}
+		then 	check_typef "$ans" || ans="A: $ans"
+			REC_OUT="${REC_OUT%%*([$IFS:])}" REC_OUT="${REC_OUT##*([$IFS:])}"
+			{	printf '%s\t%d\t"%s"\n' "${tkn[2]}" "$((max_prev<=tkn[0]?tkn[0]-max_prev:-1))" "$(escapef "${REC_OUT:-$*}")"
+				printf '%s\t%d\t"%s"\n' "${tkn[2]}" "${tkn[1]}" "$ans"
+			} >> "$FILECHAT"
+		fi; unset tkn ans
 
-	#set -- ;unset REPLY REC_OUT
-	#((OPTC)) || break
+		set --  ;unset REPLY TKN_PREV REC_OUT HIST
+		((OPTC)) || break
+	done
 fi
 
