6c394d21cce139866ec115804685ca2f115d5b2f
jsonijr@gmail.com <netosj@yahoo.com>
2023-02-28 14:37:46 -0300

    Improve Zsh `vared` prompt

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 # chatgpt.sh -- Ksh93/Bash/Zsh ChatGPT/DALL-E Shell Wrapper
-# v0.5.4  2023  by mountaineerbr  GPL+3
+# v0.5.5  2023  by mountaineerbr  GPL+3
 [[ -n $BASH_VERSION ]] && shopt -s extglob
 [[ -n $ZSH_VERSION  ]] && setopt NO_SH_GLOB KSH_GLOB KSH_ARRAYS SH_WORD_SPLIT GLOB_SUBST NO_POSIX_BUILTINS NO_NOMATCH
 
@@ -769,7 +769,7 @@ else               #completions
 					then 	unset REPLY
 						if vared -p "Prompt[${USER_TYPE:-$Q_TYPE}]: " -eh -c REPLY
 						then 	print -s - "$REPLY"
-							check_cmdf "$REPLY" && continue
+							check_cmdf "$REPLY" && continue 2
 						fi
 					else 	read -r ${BASH_VERSION:+-e}
 						check_cmdf "$REPLY" && continue
